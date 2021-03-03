bf0c5203a4763006fd6fc0eca2cda3d205285900
Ivo Santiago <ivosantiago@gmail.com>
2021-03-03 09:25:15 -0300

    P06

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import { Server } from "@overnightjs/core";
-import bodyParser from "body-parser";
-import { Application } from "express";
-import { ForecastController } from "./controllers/forecast";
-import "./util/module-alias";
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { Application } from 'express';
+import { ForecastController } from './controllers/forecast';
+import './util/module-alias';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
