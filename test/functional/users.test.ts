389f37754a9da90327a8b6b0a8833e9444dd95c2
Ivo Santiago <ivosantiago@gmail.com>
2021-03-10 10:54:46 -0300

    P03

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,4 +1,5 @@
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 describe('Users functional test', () => {
   beforeEach(async () => {
@@ -6,7 +7,7 @@ describe('Users functional test', () => {
   });
 
   describe('when creating a new user', () => {
-    it('should successfully create a new user', async () => {
+    it('should successfully create a new user with encrypted password', async () => {
       const newUser = {
         name: 'John Doe',
         email: 'john@mail.com',
@@ -15,7 +16,15 @@ describe('Users functional test', () => {
       const response = await global.testRequest.post('/users').send(newUser);
 
       expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newUser));
+      await expect(
+        AuthService.comparePasswords(newUser.password, response.body.password)
+      ).resolves.toBeTruthy();
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ password: expect.any(String) },
+        })
+      );
     });
 
     it('Should return 422 when there is a validation error', async () => {
