441d6f34abad5ba79ca3b7099ec95dbcf4067248
Ivo Santiago <ivosantiago@gmail.com>
2021-03-11 00:14:39 -0300

    P05

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,14 +1,16 @@
-import { Controller, Post } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Post } from '@overnightjs/core';
+import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
 import { BaseController } from '.';
 
 @Controller('beaches')
+@ClassMiddleware(authMiddleware)
 export class BeachesController extends BaseController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
     try {
-      const beach = new Beach(req.body);
+      const beach = new Beach({ ...req.body, ...{ user: req.decoded?.id } });
       const result = await beach.save();
       res.status(201).send(result);
     } catch (error) {
---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+import http from 'http';
+import { DecodedUser } from './services/auth';
+
+declare module 'express-serve-static-core' {
+  export interface Request extends http.IncomingMessage, Express.Request {
+    decoded?: DecodedUser;
+  }
+}
